07fd0c877a2253eddd30728efe252c3e071de8a8
Jules Torfs <julestorfs@amps.be>
2023-05-04 23:14:37 +0200

    saving updated login

--- app/routers/login.js
+++ app/routers/login.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Login = require('../models/login');
 
+// Get all users
+router.get('/', async (req, res) => {
+    // Try to get all users from the database
+    try {
+        const logins = await Login.find();
+        // Return the users
+        res.json(logins);
+    } catch (err) {
+        // If there was an error, return an error
+        res.send('Error ' + err);
+    }
+});
+
 // Register a new user
 router.post('/register', async (req, res) => {
     // Get the username, password, and email from the request
@@ -15,12 +28,18 @@ router.post('/register', async (req, res) => {
     });
 
     // Hash the password
-    login.password = await login.hashPassword(password);
+    await login.hashPassword();
+
+    // Generate a token
+    const token = await login.generateAuthToken();
 
     // Try to save the login object to the database
     try {
         const l1 = await login.save();
-        res.json(l1);
+
+        // Return the token in json format: "token": "abc123"
+        res.json({ token });
+        
     } catch (err) {
         // If there was an error, return an error
         res.send('Error');
@@ -43,24 +62,21 @@ router.post('/login', async (req, res) => {
             return res.send('User not found');
         }
 
+
         // Check if the password is not correct
-        if (!await user[0].validatePassword(password)) {
+        if (!await user[0].isValidPassword(password)) {
             return res.send('Password not correct');
         }
 
         // Generate a token
         const token = await user[0].generateAuthToken();
 
-        // Save the token to the database
-        user[0].token = token;
-        user[0].tokenExpire = Date.now() + 3600000;
-        await user[0].save();
-
         // Return the token
         res.send(token);
 
     } catch (err) {
-        res.send('Error');
+        // If there was an error, return an error with code 500
+        res.status(500).send('Error');
     }
 });
 
@@ -86,6 +102,43 @@ router.post('/logout', async (req, res) => {
         res.send('Success');
 
     } catch (err) {
+        res.send('Error');
+    }
+});
+
+// Delete a user
+router.delete('/delete', async (req, res) => {
+    // Get the token from the request
+    const { token } = req.body;
+
+    // Try to find the user in the database
+    try {
+        const user = await Login.find({ token });
+        // If the user was not found with the token, return an error
+        if (user.length === 0) {
+            return res.send('User not found');
+        }
+
+        // Remove the user from the database
+        await user[0].deleteOne();
+
+        // Return a success message
+        res.send('Success');
+
+    } catch (err) {
+        res.send('Error');
+    }
+});
+
+// Delete all users
+router.delete('/deleteAll', async (req, res) => {
+    // Try to delete all users from the database
+    try {
+        await Login.deleteMany();
+        // Return a success message
+        res.send('Success');
+    } catch (err) {
+        // If there was an error, return an error
         res.send('Error');
     }
 });
@@ -138,3 +191,5 @@ const checkToken = async (token) => {
         return false;
     }
 }
+
+module.exports = router;
